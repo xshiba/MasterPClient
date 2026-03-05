cfe16794b00b4e2b3da8994a15945926c642610c
xshiba <92438125+xshiba@users.noreply.github.com>
2026-03-05 23:26:28 +0700

    Update Private.lua

--- Private.lua
+++ Private.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/195686220a0b2c43e81703f6de047979.lua"))()
+loadstring(game:HttpGet("https://api.maruproject.online/load/proj_8cae8503-dd0b-41fe-9eb5-70cf1d1ac88d"))()
